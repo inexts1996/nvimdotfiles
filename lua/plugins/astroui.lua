b7ee81d83a2bfd327122e892182127059218d8d9
inexts1996 <inexts0618@gmail.com>
2024-07-03 19:46:29 +0800

    modify theme to modus_vivendi

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -11,7 +11,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "zenbones",
+    colorscheme = "modus_vivendi",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
