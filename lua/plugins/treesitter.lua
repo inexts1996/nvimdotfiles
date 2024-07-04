c83a5a5f7896f111b0ece7ba28af732c8de95aaa
inexts1996 <inexts0618@gmail.com>
2024-07-04 13:33:53 +0800

    modify flash and add cmdline plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,7 +9,18 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "c_sharp",
       -- add more arguments for adding more treesitter parsers
     },
+    highlight = {
+      enable = true,
+    },
+
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then return true end
+    end,
+    additional_vim_regex_highlighting = false,
   },
 }
